5581bc8bf41c5c9320d4504eb7a5d74df6b18465
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-02-19 00:58:37 -1200

    Create merge-string.cpp

---
+++ merge-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string merged;
+        int i = 0, j = 0;
+        int n1 = word1.length(), n2 = word2.length();
+
+        while (i < n1 || j < n2) {
+            if (i < n1) merged += word1[i++];
+            if (j < n2) merged += word2[j++];
+        }
+        
+        return merged;
+    }
+};
