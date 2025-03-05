2775b9baab4a471d97d59c94139f5d05586dbcf0
DIPAK PATIL <72122554+issc-dipak@users.noreply.github.com>
2025-03-05 06:20:49 -1200

    Create merge-sorted-array.cpp

---
+++ merge-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1; // Pointer for nums1 (excluding extra space)
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for the last position in nums1
+        
+        // Merge nums1 and nums2 from the end
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        
+        // Copy remaining elements from nums2 (if any)
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
